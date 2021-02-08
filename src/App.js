f2713f4d84107a19910bfaa6df1299d24b2de072
karanwadhwa <karanwadhwa0309@gmail.com>
2021-02-08 18:01:20 +0530

    BUTTONTASKWITHHOOKS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import ButtonArray from './ButtonArray';
+import Buttons from './Buttons';
 
-function App() {
+export default function App() {
+  const [text,settext] = useState("HI PRESS BUTTON");
+  const clickedHandler=(name)=>{
+    if(name===ButtonArray[3].name){
+      settext("HI PRESS BUTTON");
+    }
+    else
+      settext(`YOU CLICkED ${name}`)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{text} </h1>
+      <Buttons clicked={clickedHandler}></Buttons>
     </div>
-  );
+  )
 }
-
-export default App;
