70ac7ab16b1b861fefb286849685c402a84b0b3c
karanwadhwa <karanwadhwa0309@gmail.com>
2021-02-09 16:17:32 +0530

    USING-CONSUMER2

--- src/Child.js
+++ src/Child.js
@@ -3,23 +3,18 @@ import Buttons from "./Buttons";
 import { DataContext } from "./Context/GlobalState";
 
 export default class Child extends Component {
- // static contextType = DataContext;
-  
+  // static contextType = DataContext;
 
   render() {
     return (
       <DataContext.Consumer>
-        {
-          (context)=>
-        
-      <div>
-        <h1>{context.text}</h1>
-        <Buttons clicked={context.clicked}></Buttons>
-      </div>
-  }
+        {(context) => (
+          <div>
+            <h1>{context.text}</h1>
+            <Buttons clicked={context.clicked}></Buttons>
+          </div>
+        )}
       </DataContext.Consumer>
     );
   }
 }
-
-
