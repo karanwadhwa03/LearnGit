ef05001bf609dd65ee96847157376d7afa80ef72
karanwadhwa <karanwadhwa0309@gmail.com>
2021-02-09 13:23:34 +0530

    'ADD-HEADING-REDUX-CLASS'

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <h1>BUTTON TASK WITH REDUX CLASS</h1>
         <h1>{this.props.text}</h1>
         <Buttons clicked={this.clickHandler}></Buttons>
       </div>
